446ac1a97f940ae31c1e7f98d1a13d9b302aa2bc
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2023-05-31 15:56:47 +0200

    model: Implement gini and datacollection
    Implement gini function and collect model and agent variables

--- model/model.py
+++ model/model.py
@@ -1,8 +1,19 @@
+import matplotlib.pyplot as plt
+
 from mesa import Model
 from mesa.time import BaseScheduler
+from mesa.datacollection import DataCollector
 
 from agents import Farmer
 from objects import Farmland, Crop
+
+def calculate_gini(model):
+    agent_money = [agent.money for agent in model.schedule.agents]
+    x = sorted(agent_money)
+    N = model.num_farmers
+    B = sum(xi * (N - i) for i, xi in enumerate(x)) / (N * sum(x))
+    return 1 + (1 / N) - 2 * B
+
 
 class FarmingModel(Model):
     def __init__(self, N, crop_price):
@@ -27,11 +38,23 @@ class FarmingModel(Model):
             )
             self.schedule.add(farmer)
 
+        # Add data collector.
+        self.datacollector = DataCollector(
+            model_reporters={"Gini": calculate_gini},
+            agent_reporters={"Money": "money"}
+        )
+
     def step(self):
         self.year += 1
+        self.datacollector.collect(self)
         self.schedule.step()
 
 # Usage
 model = FarmingModel(10, 100)
 for i in range(10):
     model.step()
+    print(f"Year {model.year}: Gini = {calculate_gini(model)}")
+
+gini = model.datacollector.get_model_vars_dataframe()
+gini.plot()
+plt.show()
