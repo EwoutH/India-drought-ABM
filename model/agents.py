ade21eef352f99a17b7b30a21f2bec466bac8377
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2023-05-31 17:28:52 +0200

    model/agents: Restructure step by splitting of harvest and planting
    No functional changes, just a refactor

--- model/agents.py
+++ model/agents.py
@@ -11,13 +11,18 @@ class Farmer(Agent):
 
     def step(self):
         self.money -= self.cost_of_living
-
-        # Plant a crop if the farmland is empty
         if self.farmland.crop is not None:
-            income = self.farmland.harvest()
-            self.money += income
-            print(f"Farmer {self.unique_id} earned {income:.0f}, now has {self.money:.0f}")
-
+            self.harvest_crop()
         if self.farmland.crop is None:
-            crop = Crop(get_weighted_crop_choice())
-            self.farmland.plant(crop)
+            self.plant_crop()
+
+    def harvest_crop(self):
+        income = self.farmland.harvest()
+        self.money += income
+        print(f"Farmer {self.unique_id} earned {income:.0f}, now has {self.money:.0f}")
+
+    def plant_crop(self):
+        crop = Crop(get_weighted_crop_choice())
+        self.farmland.plant(crop)
+
+
